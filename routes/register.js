1062b1635dc666d8bd7428301a93ad37832b882e
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-04-03 14:39:53 +0530

    Added the register post data

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var mongoose = require('mongoose');
+var User = require('../app/model/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile(path.join(__dirname,'../public/register.html'));
+	res.sendfile(path.join(__dirname,'../public/register.html'));
+});
+
+router.post('/', function(req, res, next){
+	mongoose.connect('mongodb://user:user@ds015760.mlab.com:15760/librarymanagement');
+	var user = new User();
+	user.email = req.body.email;
+	user.username = req.body.username;
+	user.password = req.body.password;
+
+	user.save(function(err) {
+		if (err) {
+			// duplicate entry
+			if (err.code == 11000) 
+				return res.json({ success: false, message: 'A user with that username already exists. '});
+			else 
+				return res.send(err);
+		}
+
+		// return a message
+		res.json({ message: 'User created!' });
+	});
 });
 
 module.exports = router;

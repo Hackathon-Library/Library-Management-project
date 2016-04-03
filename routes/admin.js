52fc26d54b2fbe164d3e0bba631847404431d8c4
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-04-03 16:05:02 +0530

    Added book issue route

--- routes/admin.js
+++ routes/admin.js
@@ -3,25 +3,24 @@ var router = express.Router();
 var path = require('path');
 var mongoose = require('mongoose');
 var User = require('../app/model/user');
+var Books = require('../app/model/Books')
 
 router.post('/bookissue', function(req, res, next){
 	mongoose.connect('mongodb://user:user@ds015760.mlab.com:15760/librarymanagement');
-	var user = new User();
-	user.email = req.body.email;
-	user.username = req.body.username;
-	user.password = req.body.password;
+	var rollno = req.body.rollno;
 
-	user.save(function(err) {
-		if (err) {
-			// duplicate entry
-			if (err.code == 11000) 
-				return res.json({ success: false, message: 'A user with that username already exists. '});
-			else 
-				return res.send(err);
+	Books.find({isbn:req.body.isbn}, function(err,books) {
+		if(err) {
+			return res.send(err);
 		}
-
-		// return a message
-		res.send("<html><head><title>registered</title></head><body>User created<br /><a href='/'>back to login page</a></body></html>");
+		if(books[0].numofcopies == 0)
+			return res.send("Not enough copies");
+		else {
+			User.find({rollno:req.body.rollno}, function(err, users) {
+				
+			})
+			return res.send("Book have been issued");
+		}
 	});
 });
 

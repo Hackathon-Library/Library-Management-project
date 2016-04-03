4020f5bbefb4879fd76b2403726b725908d8b484
Avani <avaninith@gmail.com>
2016-04-03 14:11:14 +0530

    Added users schema and login form

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+mongoose = require('mongoose');
+var user = require('../app/model/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,11 +10,17 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 router.post('/', function(req, res, next){
-	if(req.body.email=='avaninith@gmail.com' && req.body.password == 'avani123'){
-		res.sendFile(path.join(__dirname, '../public/stu_fac.html'));
-	}
-	else{
-		res.sendFile(path.join(__dirname, '../public/login.html'));
-	}
+	mongoose.connect('mongodb://user:user@ds015760.mlab.com:15760/librarymanagement');
+	user.find({email:req.body.email}, function(err,users){
+		if(err)
+			return res.send(err);
+		if(req.body.password == users[0].password){
+			res.sendFile(path.join(__dirname, '../public/stu_fac.html'));
+		}
+		else{
+			res.sendFile(path.join(__dirname, '../public/login.html'));
+		}
+	});
+	
 });
 module.exports = router;

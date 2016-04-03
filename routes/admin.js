075a0484411c582f8279759627ad5d09425a008f
Avani <avaninith@gmail.com>
2016-04-03 16:39:59 +0530

    modified/admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -41,6 +41,38 @@ router.post('/newbook', function(req, res, next){
 				User.findOneAndUpdate({ book.name: 'req.body.bookname' }, { book.numofcopies: book.numofcopies + 1 }, function(err, user) {
 					if (err) throw err;
 				});
+			else 
+				return res.send(err);
+		}
+
+		// return a message
+		res.send("<html><head><title>registered</title></head><body>Book Added<br /><a href='/'>back to login page</a></body></html>");
+	});
+});
+
+router.post('/bookreturn', function(req, res, next){
+	mongoose.connect('mongodb://user:user@ds015760.mlab.com:15760/librarymanagement');
+	var book = new Book();
+	var user = new User();
+	var rollnum  = req.body.rollno;
+	var isbnno = req.body.isbn;
+	var deadline = new Date();
+	var numdaystoadd = 30;
+	deadline.setDate(user.book.issuedate + numdaystoadd)
+	user.find({ user.rollno: 'req.body.rollno' }, function(err, user) {
+					if (err) throw err;
+		var fine = Date.today() - deadline;
+		user[0].fine = fine;
+		res.render(BookReturn, {data: user[0]});
+	});
+	
+	book.save(function(err) {
+		if (err) {
+			// duplicate entry
+			if (err.code == 11000){
+				User.find({ book.ISBN: 'req.body.isbn' }, { book.numofcopies: book.numofcopies - 1 }, function(err, user) {
+					if (err) throw err;
+				});
 
 			
 
@@ -52,4 +84,6 @@ router.post('/newbook', function(req, res, next){
 		res.send("<html><head><title>registered</title></head><body>Book Added<br /><a href='/'>back to login page</a></body></html>");
 	});
 });
+
+
 module.exports = router;

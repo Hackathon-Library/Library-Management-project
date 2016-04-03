ed0171755fb6670aae1cf42ce61b28dc87a1c56f
Avani <avaninith@gmail.com>
2016-04-03 11:49:48 +0530

    added post request for login

--- routes/login.js
+++ routes/login.js
@@ -7,5 +7,12 @@ router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.sendFile(path.join(__dirname, '../public/login.html'));
 });
-
+router.post('/', function(req, res, next){
+	if(req.body.email=='avaninith@gmail.com' && req.body.password == 'avani123'){
+		res.sendFile(path.join(__dirname, '../public/stu_fac.html'));
+	}
+	else{
+		res.sendFile(path.join(__dirname, '../public/login.html'));
+	}
+});
 module.exports = router;

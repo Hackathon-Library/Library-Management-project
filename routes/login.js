6a535e43c4be1300902e5f8f0ce75dc460e9e786
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-04-07 18:16:10 +0530

    Added session

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var user = require('../app/model/user');
+var User = require('../app/model/user');
+
+var session;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  res.sendFile(path.join(__dirname, '../public/login.html'));
+
+	session = req.session;
+	console.log(session);
+	if(session.email) {
+		User.findOne({email:session.email}, function(err,user){
+			if(err)
+				return res.send(err);
+			if(user.role=="admin")
+				res.render('Admin', {data:user});
+			else
+				res.render('stu_fac', {data:user});
+		});
+	}
+	else
+		res.sendFile(path.join(__dirname, '../public/login.html'));
 
 });
 
 router.post('/', function(req, res, next){
 	
-	user.find({email:req.body.email}, function(err,users){
+	session = req.session;
+
+	User.findOne({email:req.body.email}, function(err,user){
 		if(err)
 			return res.send(err);
-		console.log(users[0]);
-		if(req.body.password == users[0].password){
-			if(users[0].role=="admin")
-				res.render('Admin', {data:users[0]});
+		if(req.body.password == user.password){
+
+			session.email = req.body.email;
+
+			if(user.role=="admin")
+				res.render('Admin', {data:user});
 			else
-				res.render('stu_fac', {data:users[0]});
+				res.render('stu_fac', {data:user});
 		}
 		else{
 			res.sendFile(path.join(__dirname, '../public/login.html'));

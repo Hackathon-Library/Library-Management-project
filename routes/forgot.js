37c7d7dda10bccb8be3d38a2df4c23e95571f795
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-04-16 23:51:40 +0530

    bug fix

--- routes/forgot.js
+++ routes/forgot.js
@@ -42,7 +42,7 @@ router.post('/', function(req,res,next) {
 			});
 			var mailOptions = {
 				to: user.email,
-				from: 'passwordreset@demo.com',
+				from: 'fakehimanshusingh@gmail.com',
 				subject: 'NITH Library Login Password Reset',
 				text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
 				'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
